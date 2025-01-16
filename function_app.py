23377ef7e9caf574db48b718122af1a5d3f79f94
Antonio Resca <antonio.resca@gmail.com>
2025-01-16 10:18:40 +0100

    first commit

---
+++ function_app.py
@@ -0,0 +1,26 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="doppio")
+def doppio(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    value = req.params.get('value')
+    if not value:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            value = req_body.get('value')
+
+    if value:
+        valuex2=value*2
+        return func.HttpResponse(f"il doppio di {value} è {valuex2}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
